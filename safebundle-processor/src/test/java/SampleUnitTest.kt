8b9717dcc0283d53c17be17ea04587e388b3d000
Yonghun Kim <sabgilhun@gmail.com>
2021-03-29 02:44:13 +0900

    add unit test

---
+++ safebundle-processor/src/test/java/SampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.sabgil.processor
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 1 + 2)
+    }
+}

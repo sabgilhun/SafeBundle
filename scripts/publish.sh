f0dd1bd9f04dfa9fc8825320ad061ececb0fea84
Yonghun Kim <sabgilhun@gmail.com>
2021-03-29 00:51:18 +0900

    divide publish.sh into build.sh publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-./gradlew :safebundle-annotation:jar \
-  :safebundle-processor:jar \
-  :safebundle:assemble \
-
 ./gradlew :safebundle-annotation:publishReleasePublicationToCentralRepository \
   :safebundle-processor:publishReleasePublicationToCentralRepository \
   :safebundle:publishReleasePublicationToCentralRepository
